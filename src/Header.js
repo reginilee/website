7dba7a816a2d515bc99b14513eb3f39b9061e812
reginilee <e0313703@u.nus.edu>
2022-10-14 16:46:14 +0800

    Added Header

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <section className="header">
+            <h1>Hi! My name is <strong>Regini Lee.</strong><br/>
+            I am a designer and a developer.</h1>
+        </section>
+    )
+}
