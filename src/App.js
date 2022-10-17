1578c5fdf88a3b2dbba6b661d8a476a5063a4e34
reginilee <e0313703@u.nus.edu>
2022-10-17 17:04:58 +0800

    Added Fullpage.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,56 @@ import "./App.css";
 import Header from "./Header";
 import Project from "./Project";
 import { Project_01, Project_02, Project_03, Project_04, Project_05, Project_06 } from "./Project_Data";
+import ReactFullpage from '@fullpage/react-fullpage';
 
-function App() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <div className="App">
-      <Header />
-      <Project {...Project_01} />
-      <Project {...Project_02} />
-      <Project {...Project_03} />
-      <Project {...Project_04} />
-      <Project {...Project_05} />
-      <Project {...Project_06} />
-    </div>
-  );
-}
+const App = () => (
+  // useEffect(() => {
+  //   window.scrollTo(0, 0);
+  // }, []);
+
+  <ReactFullpage
+  licenseKey = {''}
+  scrollingSpeed = {5000}
+   render={({ state, fullpageApi }) => {
+    return (
+      <ReactFullpage.Wrapper>
+        <div className="section">
+          <Header />
+        </div>
+        <div className="section">
+          <Project {...Project_01} />
+        </div>
+        <div className="section">
+          <Project {...Project_02} />
+        </div>
+        <div className="section">
+          <Project {...Project_03} />
+        </div>
+        <div className="section">
+          <Project {...Project_04} />
+        </div>
+        <div className="section">
+          <Project {...Project_05} />
+        </div>
+        <div className="section">
+          <Project {...Project_06} />
+        </div>
+      </ReactFullpage.Wrapper>
+    );
+   }}
+  />
+
+  // return (
+  //   <div className="App">
+  //     <Header />
+  //     <Project {...Project_01} />
+  //     <Project {...Project_02} />
+  //     <Project {...Project_03} />
+  //     <Project {...Project_04} />
+  //     <Project {...Project_05} />
+  //     <Project {...Project_06} />
+  //   </div>
+  // );
+);
 
 export default App;
