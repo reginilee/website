613517779d3a286616d8f7762813633784bc8a91
reginilee <e0313703@u.nus.edu>
2022-10-17 23:09:42 +0800

    Minimised images

--- src/App.js
+++ src/App.js
@@ -1,61 +1,52 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Project from "./Project";
-import { Project_01, Project_02, Project_03, Project_04, Project_05, Project_06 } from "./Project_Data";
-import ReactFullpage from '@fullpage/react-fullpage';
+import {
+  Project_01,
+  Project_02,
+  Project_03,
+  Project_04,
+  Project_05,
+  Project_06,
+} from "./Project_Data";
+import ReactFullpage from "@fullpage/react-fullpage";
 import Footer from "./Footer";
 
 const App = () => (
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, []);
-
   <ReactFullpage
-  licenseKey = {''}
-   render={({ state, fullpageApi }) => {
-    return (
-      <ReactFullpage.Wrapper>
-        <div className="section">
-          <Header />
-        </div>
-        <div className="section">
-          <Project {...Project_01} />
-        </div>
-        <div className="section">
-          <Project {...Project_02} />
-        </div>
-        <div className="section">
-          <Project {...Project_03} />
-        </div>
-        <div className="section">
-          <Project {...Project_04} />
-        </div>
-        <div className="section">
-          <Project {...Project_05} />
-        </div>
-        <div className="section">
-          <Project {...Project_06} />
-        </div>
-        <div className="section">
-          <Footer />
-        </div>
-      </ReactFullpage.Wrapper>
-    );
-   }}
+    licenseKey={""}
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section">
+            <Header />
+          </div>
+          <div className="section">
+            <Project {...Project_01} />
+          </div>
+          <div className="section">
+            <Project {...Project_02} />
+          </div>
+          <div className="section">
+            <Project {...Project_03} />
+          </div>
+          <div className="section">
+            <Project {...Project_04} />
+          </div>
+          <div className="section">
+            <Project {...Project_05} />
+          </div>
+          <div className="section">
+            <Project {...Project_06} />
+          </div>
+          <div className="section">
+            <Footer />
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
   />
-
-  // return (
-  //   <div className="App">
-  //     <Header />
-  //     <Project {...Project_01} />
-  //     <Project {...Project_02} />
-  //     <Project {...Project_03} />
-  //     <Project {...Project_04} />
-  //     <Project {...Project_05} />
-  //     <Project {...Project_06} />
-  //   </div>
-  // );
 );
 
 export default App;
