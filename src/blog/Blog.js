6f0d21a55260aaab260c0b560c81787de70a52df
reginilee <e0313703@u.nus.edu>
2022-11-01 11:17:49 +0800

    Added new blog data

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BlogPost from "./BlogPost";
 import "./Blog.css";
-import { Post_01, Post_02, Post_03, Post_04, Post_05, Post_06 } from "./Blog_Data";
+import { Post_01, Post_02, Post_03, Post_04, Post_05, Post_06, Post_07, Post_08, Post_09 } from "./Blog_Data";
 import Footer from "../footer/Footer";
 
 export default function Blog() {
@@ -9,14 +9,19 @@ export default function Blog() {
     <>
     <section className="blogpage">
         <div className="blog">
-        <BlogPost {...Post_01} />
-        <BlogPost {...Post_02} />
-        <BlogPost {...Post_03} />
+        <BlogPost {...Post_09} />
+        <BlogPost {...Post_08} />
+        <BlogPost {...Post_07} />
         </div>
         <div className="blog">
-        <BlogPost {...Post_04} />
-        <BlogPost {...Post_05} />
         <BlogPost {...Post_06} />
+        <BlogPost {...Post_05} />
+        <BlogPost {...Post_04} />
+        </div>
+        <div className="blog">
+        <BlogPost {...Post_03} />
+        <BlogPost {...Post_02} />
+        <BlogPost {...Post_01} />
         </div>
     </section>
     <Footer />
--- src/blog/Blog_Data.js
+++ src/blog/Blog_Data.js
@@ -1,36 +1,11 @@
 export const Post_01 = {
-  img_src: require("../image/blog/Banana_Pancake.jpg"),
-  img_alt_text:
-    "A cup of coffee behind a stack of banana pancakes stuffed with banana slices.",
-  caption:
-    "Made banana pancakes stuffed with banana slices. And made my own coffee with a portable coffee machine!",
-  date: "15 October 2022",
+  img_src: require("../image/blog/Langkawi_Sunset.jpg"),
+  img_alt_text: "Sunset at a Langkawi beach, Malaysia.",
+  caption: "Had one of the best trips with one of my very close frends.",
+  date: "2 June 2022",
 };
 
 export const Post_02 = {
-  img_src: require("../image/blog/Painting_Whale.jpg"),
-  img_alt_text: "Watercolor painting of a whale jumping over a ship.",
-  caption:
-    "Started on a 'Painting by Numbers' kit of a whale jumping over a ship. This is going to take a while.",
-  date: "5 October 2022",
-};
-
-export const Post_03 = {
-  img_src: require("../image/blog/Chicken_Wrap.jpg"),
-  img_alt_text: "A cup of coffee next to a chicken wrap.",
-  caption:
-    "Made a chicken wrap for a low-calorie, high-protein meal. Working from home is the norm now.",
-  date: "5 October 2022",
-};
-
-export const Post_04 = {
-  img_src: require("../image/blog/Miniature_Dollhouse.jpg"),
-  img_alt_text: "Pieces of a dismantled miniature dollhouse.",
-  caption: "Excited to start building another miniature dollhouse!",
-  date: "3 October 2022",
-};
-
-export const Post_05 = {
   img_src: require("../image/blog/Shakshuka_With_Toast.jpg"),
   img_alt_text: "Shakshuka with two pieces of toasts.",
   caption:
@@ -38,9 +13,61 @@ export const Post_05 = {
   date: "29 September 2022",
 };
 
+export const Post_03 = {
+  img_src: require("../image/blog/Miniature_Dollhouse.jpg"),
+  img_alt_text: "Pieces of a dismantled miniature dollhouse.",
+  caption: "Excited to start building another miniature dollhouse!",
+  date: "3 October 2022",
+};
+
+export const Post_04 = {
+  img_src: require("../image/blog/Chicken_Wrap.jpg"),
+  img_alt_text: "A cup of coffee next to a chicken wrap.",
+  caption:
+    "Made a chicken wrap for a low-calorie, high-protein meal. Working from home is the norm now.",
+  date: "5 October 2022",
+};
+
+export const Post_05 = {
+  img_src: require("../image/blog/Painting_Whale.jpg"),
+  img_alt_text: "Watercolor painting of a whale jumping over a ship.",
+  caption:
+    "Started on a 'Painting by Numbers' kit of a whale jumping over a ship. This is going to take a while.",
+  date: "5 October 2022",
+};
+
 export const Post_06 = {
-  img_src: require("../image/blog/Langkawi_Sunset.jpg"),
-  img_alt_text: "Sunset at a Langkawi beach, Malaysia.",
-  caption: "Had one of the best trips with one of my very close frends.",
-  date: "2 June 2022",
+  img_src: require("../image/blog/Banana_Pancake.jpg"),
+  img_alt_text:
+    "A cup of coffee behind a stack of banana pancakes stuffed with banana slices.",
+  caption:
+    "Made banana pancakes stuffed with banana slices. And made latte with a portable coffee machine!",
+  date: "15 October 2022",
+};
+
+export const Post_07 = {
+  img_src: require("../image/blog/Painting_Whale_Complete.jpg"),
+  img_alt_text:
+    "Display of a framed watercolor painting of a whale jumping over a ship on a shelf.",
+  caption:
+    "Completed the 'Painting by Numbers' kit of a whale jumping over a ship.",
+  date: "24 October 2022",
+};
+
+export const Post_08 = {
+  img_src: require("../image/blog/Picnic.jpg"),
+  img_alt_text:
+    "Roti prata, banana, grapes, mooncake, kaya and butter toast, sushi and a roll cake on a pinic mat.",
+  caption:
+    "Unconventional picnic food. Then again, although this friend group has different personalities and interests, we still clicked well.",
+  date: "29 October 2022",
+};
+
+export const Post_09 = {
+  img_src: require("../image/blog/Miniature_Kitchen.jpg"),
+  img_alt_text:
+    "Miniature kitchen set.",
+  caption:
+    "Started building the miniature dollhouse.",
+  date: "31 October 2022",
 };
